914b04e6d28ea166339d7ca5cbe4c8b7b50c875a
paulhubacek1-afk <paul.hubacek1@gmail.com>
2026-09-21 17:33:51 +0200

    feat: add iOS IPA build setup

---
+++ Package.swift
@@ -0,0 +1,2 @@
+import PackageDescription
+let package = Package(name:"GlassClip", platforms:[.iOS(.v16)], products:[.library(name:"GlassClip",targets:["GlassClip"])], targets:[.target(name:"GlassClip")])
